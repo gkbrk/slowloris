fcff598745252628af12cc438fcf2ca569b0f567
Gökberk Yaltıraklı <webdosusb@gmail.com>
2015-04-26 12:59:51 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 setup(
     name = "Slowloris",
     py_modules = ["slowloris"],
+    entry_points = {"console_scripts": ["slowloris=slowloris:main"]},
     version = "0.1",
     description = "Low bandwidth DoS tool. Slowloris rewrite in Python.",
     author = "Gokberk Yaltirakli",
