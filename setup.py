0284d46f9f8d2011d0e5388d27bcb20797d0b53e
Gokberk Yaltirakli <git@gkbrk.com>
2020-03-27 13:01:20 +0000

    pypi: Bump package version to v0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="Slowloris",
     py_modules=["slowloris"],
     entry_points={"console_scripts": ["slowloris=slowloris:main"]},
-    version="0.2.0",
+    version="0.2.1",
     description="Low bandwidth DoS tool. Slowloris rewrite in Python.",
     author="Gokberk Yaltirakli",
     author_email="webdosusb@gmail.com",
